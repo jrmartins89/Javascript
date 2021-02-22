865875fd998eaab73155bd5538972bdd823659c0
Jose Ribamar Marcal Martins Junior <73797262+jrmartins89@users.noreply.github.com>
2021-02-22 19:31:54 -0300

    Dynamic stack V2
    This is the implementation of a stack using dynamic memory allocation. Files:
    - index.js => Main file of the stack;
    - pilha.js => Stack class with its methods;
    - elemento.js => Node class with its methods;

---
+++ pilhadinamica/elemento.js
@@ -0,0 +1,24 @@
+class Elemento{
+  constructor(valor){
+    this.valor = valor;
+    this.proximo=null;
+  }
+
+  getValor(){
+    return this.valor;
+  }
+
+  getProximo(){
+    return this.proximo;
+  }
+
+  setValor(valor){
+    this.valor = valor;
+  }
+
+  setProximo(proximo){
+    this.proximo = proximo;
+  }
+}
+
+module.exports = Elemento
